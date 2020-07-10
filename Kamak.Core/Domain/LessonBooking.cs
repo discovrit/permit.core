a7dcf125c482b4ce882ac7a10659fc842818b0fb
michaelgreenhalgh <michael@wotwmedia.com>
2020-07-10 23:25:20 +0100

    Added Lesson Booking object and included csproj for Core

---
+++ Kamak.Core/Domain/LessonBooking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Kamak.Core.Domain
+{
+    public class LessonBooking
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+    }
+}
